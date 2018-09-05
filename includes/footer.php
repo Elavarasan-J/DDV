5a8f6fce7031d8bbd660d4d37e43c50ec34ef7f5
Elavarasan-J <elavarasan.infotech10@gmail.com>
2018-09-05 22:24:26 +0530

    'footer'

--- includes/footer.php
+++ includes/footer.php
@@ -1,171 +1,62 @@
-
-		<!--Footer -->
-		<footer class="footer" style="display:none;">
-			<div class="container">
-				<div class="footer-top">
-					<!--<div class="footer-logo">
-						<img src="images/logo.png" alt="Bloom">
-					</div>-->
-					
-					<div class="row">
-						<div class="col-sm-5 col-md-6">
-							<div id="footer-map"></div>		
-							
-						</div>
-						<div class="col-sm-4 col-md-3">
-							<div class="footer-col">
-								<h4 class="footer-title">Hours</h4>
-								<ul class="hours-list">
-									<li><span>Monday</span> 9am - 5pm</li>
-									<li><span>Tuesday</span> 9am - 9pm</li>
-									<li><span>Wednesday</span> 9am - 9pm</li>
-									<li><span>Thursday</span> 9am - 9pm</li>
-									<li><span>Friday</span> 9am - 6pm</li>
-									<li><span>Saturday</span> 9am - 5pm</li>
-									<li><span>Sunday</span> 10am - 4pm</li>
-								</ul>
-							</div>
-						</div>
-						<div class="col-sm-3 col-md-3">
-							<div class="footer-col">
-								<h4 class="footer-title">Contact Us</h4>
-								<div class="footer-contact">
-									<p>2 Maple Ave, Morristown, NJ 07960</p>
-								</div>
-
-								<div class="footer-contact">
-									<h4 class="footer-title">Phone</h4>
-									<p>(973)292-3377</p>
-								</div>
-
-								<div class="footer-contact">
-									<h4 class="footer-title">Email</h4>
-									<p>info@bloommorristown.com</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				
-				<div class="footer-bottom">
-					<div class="row">
-						<div class="col-sm-6">
-							<p class="footer-bottom-links"><a href="#">Terms of Use</a><a href="#">Privacy Policy</a></p>
-						</div>
-						<div class="col-sm-6">
-							<p class="copyright">2018&copy; Bloom | All Rights Reserved</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer><!--/Footer -->
-
-		</div>
-        <!--/Wrapper -->    
-	   
-		<script src="js/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-	   	<script src="js/feather.min.js"></script>
-<?php if(isset($datapicker) && ($datapicker == 1)) { ?>
-		<script src="js/moment.min.js"></script>
-		<script src="js/bootstrap-datetimepicker.min.js"></script>
-		<script>      
-			// Datepicker
-			$(function () {
-				$('.datepicker').datetimepicker({
-					format: 'MM/DD/YYYY'
-				});
-				
-				$('.datetime-picker').datetimepicker();
-			});
-	   </script>
-
-<?php } ?>
-		<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIz1XcGXhRyDpUkvSm2QPipB-l2kOu5sQ&callback=initMap"></script>
-		<script>
-			// Footer Map
-			function initMap() {
-				var myStyles =[{
-					featureType: "poi",
-					elementType: "labels",
-					stylers: [{ visibility: "off" }]
-				}];
-
-				var map = new google.maps.Map(document.getElementById('footer-map'), {
-					zoom: 17,
-					center: {lat: 40.795479, lng: -74.482792},
-					styles: myStyles 
-				});
-
-				var image = 'images/map-marker.png',
-					contentString = '<div class="custom-marker-content"><h3>Bloom</h3><p>2 Maple Ave, Morristown, NJ 07960, USA</p></div>',
-					infowindow = new google.maps.InfoWindow({content: contentString});
-
-				var marker = new google.maps.Marker({
-					position: {lat: 40.795479, lng: -74.482792},
-					map: map,
-					title:"2 Maple Ave, Morristown, NJ 07960, USA",
-					icon: image
-				});
-
-				marker.addListener('click', function() {
-					infowindow.open(map, marker);
-				});
-			}
-		</script>
-		<script src="js/main.js"></script>
-
-		<?php if(isset($home_page) && ($home_page == true)) { ?>
-		<script src="js/jquery.superslides.min.js"></script>
-		<script src="js/slick.min.js"></script>
-		<script>
-			$('#slides').superslides({
-			  play: 8000,
-			  animation: 'fade',
-			  animation_speed: 'slow'
-			});
-			
-			$('body').on('animated.slides', '#slides', function(e){
-				$('.slides-item').removeClass('zoomin');
-				var currentSlide = $('#slides').superslides('current');
-				$('.slides-container').children().eq(currentSlide).addClass('zoomin');
-			});
-			 
-			 
-
-			// Banner Slick Carousel
-			$('.instagram-slick').slick({
-			  centerMode: true,
-			  slidesToShow: 6,
-			  slidesToScroll: 1,
-			  autoplay:true,
-			  autoplaySpeed: 2200,
-			  arrows: false,
-			  swipe:false,
-			  touchMove : true,
-			  responsive: [
-				  {
-				  breakpoint: 1200,
-				  settings: {
-					slidesToShow: 4
-				  }
-				},
-				{
-				  breakpoint: 1024,
-				  settings: {
-					slidesToShow: 3
-				  }
-				},
-				{
-				  breakpoint: 600,
-				  settings: {
-					swipe:true,
-					slidesToShow: 2
-				  }
-				}
-			  ]
-			});
-		</script>
-		<?php } ?>
-	</body>
-</html>
+<footer class="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="footer-col">
+                        <h4 class="footer-title">About Us</h4>
+                        <hr class="line50">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the text ever since 1500s.</p>
+                        <ul class="social-list">
+                            <li><a class="fa fa-facebook" href="#"></a></li>
+                            <li><a class="fa fa-twitter" href="#"></a></li>
+                            <li><a class="fa fa-instagram" href="#"></a></li>
+                            <li><a class="fa fa-youtube-play" href="#"></a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="col">
+                    <div class="footer-col pl50">
+                        <h4 class="footer-title">quick links</h4>
+                        <hr class="line50">
+                        <ul class="footer-list">
+                             <li><a href="#">About Us</a></li>
+                             <li><a href="#">Our Products</a></li>
+                             <li><a href="#">Contact Us</a></li>
+                             <li><a href="#">Enquire Now</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="col">
+                    <div class="footer-col">
+                        <h4 class="footer-title">Address</h4>
+                        <hr class="line50">
+                        <address>No:78, Middle Street,<br> Othiyadikuppam, Vellakarai (Post), <br>Cuddalore - 607003. <br>Tamilnadu, India</address>
+                        <img src="images/india-flag.png">
+                    </div>
+                </div>
+                
+                <div class="col">
+                    <div class="footer-col">
+                        <h4 class="footer-title">Phone</h4>
+                        <hr class="line50">
+                        <p>+91-7339013805</p>
+                        <br>
+                        
+                        <h4 class="footer-title">Email</h4>
+                        <hr class="line50">
+                        <p><i class="fa fa-phone"></i> ddvexports@yahoo.com</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <p>Copyright © 2018 Artistic Academy</p>
+        </div>
+    </div>
+</footer>
